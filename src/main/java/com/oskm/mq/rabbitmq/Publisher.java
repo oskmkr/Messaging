fe5ab7b6839b12b2f3449b18c47938afea48a1ca
oskmkr <oskmkr@naver.com>
2014-08-01 17:54:55 +0900

    formatting

--- src/main/java/com/oskm/mq/rabbitmq/Publisher.java
+++ src/main/java/com/oskm/mq/rabbitmq/Publisher.java
@@ -1,7 +1,58 @@
 package com.oskm.mq.rabbitmq;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.apache.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by sungkyu.eo on 2014-08-01.
  */
 public class Publisher {
+
+    private static final Logger LOG = Logger.getLogger(Publisher.class);
+    private static final String QUEUE_NAME = "hello_rabbit_mq";
+
+    public static void main(String[] args) throws IOException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+            for (; ; ) {
+                String line = in.readLine();
+                if (line == null) {
+                    break;
+                }
+
+                String message = "Hello, rabbit MQ";
+
+                channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+
+                LOG.debug(" [x] Sent '" + message + "'");
+
+
+                // If user typed the 'bye' command, wait until the server closes
+                // the connection.
+                if ("bye".equals(line.toLowerCase())) {
+                    break;
+                }
+            }
+        } finally {
+            channel.close();
+            connection.close();
+        }
+    }
+
+
 }
